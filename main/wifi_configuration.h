66837c4d6a1a88c8b33d2e26bc9455e14b7ea474
yumura <yumura@example.com>
2026-07-20 09:26:26 +0800

    build target

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -15,7 +15,7 @@ static struct {
     const char *ssid;
     const char *password;
 } wifi_list[] __attribute__((unused)) = {
-    {.ssid = "OTA", .password = "12345678"},
+    {.ssid = "OTA", .password = "0d000721"},
     {.ssid = "DAP", .password = "12345678"},
     // Add your WAP like this:
     {.ssid = "YUMURA", .password = "0d000721"},
